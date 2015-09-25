f0d26fae993c4c05d4ebd2ab04c69d481b41bdc3
Zsolt Gál <zsolt.gal@sessiondigital.com>
2015-09-25 13:55:26 +0200

    Update build.sh to copy built jira to dropbox

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-phar-composer build && mv jira.phar /usr/local/bin/jira && chmod u+x /usr/local/bin/jira
+echo "Build composer.phar, make it executable and move it to /usr/local/bin/jira, copy builded phar to dropbox"
+phar-composer build && mv jira.phar /usr/local/bin/jira && chmod u+x /usr/local/bin/jira && cp /usr/local/bin/jira ~/Dropbox/jira.phar
