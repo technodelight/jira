ad4d6d268310c3609026d4df31eff9c46f66d729
Zsolt Gál <zsolt.gal@sessiondigital.com>
2017-02-16 14:34:24 +0100

    Change build a bit

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Build composer.phar, make it executable and move it to /usr/local/bin/jira, copy builded phar to dropbox"
+git describe --abbrev=0 --tags > bin/.version
 phar-composer build && mv jira.phar /usr/local/bin/jira && chmod +x /usr/local/bin/jira && cp /usr/local/bin/jira ~/Dropbox/jira.phar
