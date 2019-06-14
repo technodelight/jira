f1439a84376a03172e47b3bb312d1f59af78a02c
Zsolt Gál <zsolt.gal@sessiondigital.com>
2019-06-14 16:45:09 +0200

    Release 0.9.18
    - refactor
    - remove daemon option

--- src/Technodelight/Jira/Console/Application.php
+++ src/Technodelight/Jira/Console/Application.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Technodelight\Jira\Console\Application\Daemon;
 use Technodelight\Jira\Helper\PluralizeHelper;
 
 class Application extends BaseApp
@@ -17,14 +16,6 @@ class Application extends BaseApp
      * @var ContainerBuilder
      */
     private $container;
-    /**
-     * @var string
-     */
-    private $daemonFilename;
-    /**
-     * @var bool
-     */
-    private $isDaemon = false;
     /**
      * @var string
      */
@@ -37,11 +28,6 @@ class Application extends BaseApp
         }
     }
 
-    public function setDaemonFilename($filename)
-    {
-        $this->daemonFilename = $filename;
-    }
-
     public function currentInstanceName()
     {
         return $this->currentInstanceName;
@@ -51,28 +37,7 @@ class Application extends BaseApp
     {
         $this->handleRuntimeVariables($input);
 
-        if ($input->hasParameterOption(['--daemon', '-D'])) {
-            $this->isDaemon = true;
-
-            Daemon::setFilename(__FILE__);
-            Daemon::getInstance()->setApplication($this);
-
-            $config = $this->container->get('technodelight.jira.config.integrations.daemon');
-
-            if ($input->hasParameterOption(['--ip'])) {
-                Daemon::getInstance()->setAddress($input->getParameterOption(['--ip']));
-            } else {
-                Daemon::getInstance()->setAddress($config->address());
-            }
-            if ($input->hasParameterOption(['--port'])) {
-                Daemon::getInstance()->setPort($input->getParameterOption(['--port']));
-            } else {
-                Daemon::getInstance()->setPort($config->port());
-            }
-            Daemon::getInstance()->run();
-        } else {
-            return parent::run($input, $output);
-        }
+        return parent::run($input, $output);
     }
 
     public function doRun(InputInterface $input, OutputInterface $output)
@@ -80,14 +45,14 @@ class Application extends BaseApp
         if (true === $input->hasParameterOption(['--debug', '-d'])) {
             $start = microtime(true);
             $startMem = memory_get_usage(true);
-            $result = $this->runWithClientOrApp($input, $output);
+            $result = parent::doRun($input, $output);
             $end = microtime(true) - $start;
             $endMem = memory_get_peak_usage(true);
             $output->writeLn(sprintf('%1.4f s, mem %s', $end, $this->formatBytes($endMem - $startMem)));
 
             return $result;
         } else {
-            return $this->runWithClientOrApp($input, $output);
+            return parent::doRun($input, $output);
         }
     }
 
@@ -122,24 +87,12 @@ BANNER;
             . 'See https://github.com/technodelight/jira/blob/master/LICENSE.';
     }
 
-    protected function runWithClientOrApp(InputInterface $input, OutputInterface $output)
-    {
-        if ($this->container->get('technodelight.jira.config.integrations.daemon')->enabled() && !$this->isDaemon) {
-            $this->container->get('technodelight.jira.console.daemon.client')->run($input, $output);
-        } else {
-            return parent::doRun($input, $output);
-        }
-    }
-
     protected function getDefaultInputDefinition()
     {
         $input = parent::getDefaultInputDefinition();
         $input->addOption(new InputOption('--debug', null, InputOption::VALUE_NONE, 'Enable debug mode'));
         $input->addOption(new InputOption('--instance', '-i', InputOption::VALUE_REQUIRED, 'Use an instance from config temporarily'));
         $input->addOption(new InputOption('--no-cache', '-N', InputOption::VALUE_NONE, 'Cleare app cache before running command'));
-        $input->addOption(new InputOption('--daemon', null, InputOption::VALUE_NONE, 'Daemonise app'));
-        $input->addOption(new InputOption('--port', null, InputOption::VALUE_REQUIRED, 'Port to use for daemon'));
-        $input->addOption(new InputOption('--ip', null, InputOption::VALUE_REQUIRED, 'Address to use for daemon'));
 
         return $input;
     }
@@ -159,14 +112,6 @@ BANNER;
     private function handleRuntimeVariables(InputInterface $input)
     {
         $this->currentInstanceName = $input->getParameterOption(['--instance', '-i']) ?: 'default';
-
-        $daemonRuntimeConfig = $this->container->get('technodelight.jira.console.daemon.client.runtime_configuration');
-        if ($input->hasParameterOption(['--ip'])) {
-            $daemonRuntimeConfig->setAddress($input->getParameterOption(['--ip']));
-        }
-        if ($input->hasParameterOption(['--port'])) {
-            $daemonRuntimeConfig->setPort($input->getParameterOption(['--port']));
-        }
 
         if (true === $input->hasParameterOption(['--no-cache', '-N'])) {
             /** @var \ICanBoogie\Storage\Storage $cache */
---
+++ src/Technodelight/Jira/Renderer/Action/Issue/Edit/Renderer.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Technodelight\Jira\Renderer\Action\Issue\Edit;
+
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Output\OutputInterface;
+use Technodelight\Jira\Api\JiraRestApi\Api;
+use Technodelight\Jira\Renderer\Action\Renderer as ActionRenderer;
+use Technodelight\Jira\Renderer\Action\Result;
+use Technodelight\Jira\Renderer\Action\StyleGuide;
+use Technodelight\Jira\Renderer\Issue\Header;
+use Technodelight\Jira\Renderer\Issue\IssueRelations;
+
+class Renderer implements ActionRenderer
+{
+    /**
+     * @var Api
+     */
+    private $api;
+    /**
+     * @var Header
+     */
+    private $headerRenderer;
+    /**
+     * @var IssueRelations
+     */
+    private $issueRelationsRenderer;
+    /**
+     * @var FormatterHelper
+     */
+    private $formatterHelper;
+    /**
+     * @var StyleGuide
+     */
+    private $styleGuide;
+
+    public function __construct(
+        Api $api,
+        Header $headerRenderer,
+        IssueRelations $issueRelationsRenderer,
+        FormatterHelper $formatterHelper,
+        StyleGuide $styleGuide
+    )
+    {
+        $this->api = $api;
+        $this->headerRenderer = $headerRenderer;
+        $this->issueRelationsRenderer = $issueRelationsRenderer;
+        $this->formatterHelper = $formatterHelper;
+        $this->styleGuide = $styleGuide;
+    }
+
+    public function canProcess(Result $result): bool
+    {
+        return $result instanceof Success || $result instanceof Error;
+    }
+
+    public function render(OutputInterface $output, Result $result): int
+    {
+        if ($result instanceof Success) {
+            return $this->renderSuccess($output, $result);
+        }
+
+        if ($result instanceof Error) {
+            return $this->renderError($output, $result);
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param Result $result
+     * @return int
+     */
+    protected function renderSuccess(OutputInterface $output, Success $result): int
+    {
+        $issue = $this->api->retrieveIssue($result->issueKey());
+        $this->headerRenderer->render($output, $issue);
+        $this->issueRelationsRenderer->render($output, $issue);
+
+        $output->writeln([
+            '',
+            vsprintf(
+                $result->phrase(),
+                array_filter(
+                    [
+                        $this->styleGuide->formatIssueKey($result->issueKey()),
+                        $this->styleGuide->formatTransition($result->data()[0]),
+                        !empty($result->data()[1]) ? $this->styleGuide->formatUsername($result->data()[1]) : null,
+                    ]
+                )
+            )
+        ]);
+
+        return 0;
+    }
+
+    private function renderError(OutputInterface $output, Error $error): int
+    {
+        if ($output->getVerbosity() == OutputInterface::VERBOSITY_QUIET) {
+            return $error->exception()->getCode() ?: 1;
+        }
+
+        $output->writeln(
+            $this->formatterHelper->formatBlock(
+                vsprintf($error->phrase(), array_filter(array_merge([$error->issueKey()], $error->data()))),
+                'error',
+                true
+            )
+        );
+        $output->writeln('');
+        $issue = $this->api->retrieveIssue($error->issueKey());
+        $this->headerRenderer->render($output, $issue);
+        $this->issueRelationsRenderer->render($output, $issue);
+
+        if ($output->getVerbosity() == OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln($error->exception()->getTraceAsString()); //@TODO: a nice formatting would be good here
+        }
+
+        return $error->exception()->getCode() ?: 1;
+    }
+}
