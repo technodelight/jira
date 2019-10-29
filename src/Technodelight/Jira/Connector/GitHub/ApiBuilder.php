c14c7f412fcea7b4cc8214880f55c7b38725a241
Zsolt Gál <zsolt.gal@sessiondigital.com>
2019-10-29 16:17:32 +0100

    Fix self-update github API

--- src/Technodelight/Jira/Connector/GitHub/ApiBuilder.php
+++ src/Technodelight/Jira/Connector/GitHub/ApiBuilder.php
@@ -5,23 +5,13 @@ namespace Technodelight\Jira\Connector\GitHub;
 use Buzz\Client\MultiCurl;
 use Github\Client;
 use Github\HttpClient\Builder;
-use Technodelight\Jira\Configuration\ApplicationConfiguration\IntegrationsConfiguration\GitHubConfiguration;
 
 class ApiBuilder
 {
-    private $configuration;
-
-    public function __construct(GitHubConfiguration $configuration)
+    public static function build()
     {
-        $this->configuration = $configuration;
-    }
-
-    public function build()
-    {
-        $client = new Client(
+        return new Client(
             new Builder(new MultiCurl())
         );
-        $client->authenticate($this->configuration->token(), null, Client::AUTH_URL_TOKEN);
-        return $client;
     }
 }
