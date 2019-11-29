2e0c9ae0441b1a87d47f72563ac6fcc3cedd5523
Zsolt Gál <zsolt.gal@sessiondigital.com>
2019-11-29 14:40:58 +0100

    remove internal jira tag converter

--- src/Technodelight/JiraGitHubExtension/Extension.php
+++ src/Technodelight/JiraGitHubExtension/Extension.php
@@ -1,58 +1,72 @@
 <?php
 
-namespace Technodelight\JiraGitHubExtension;
+namespace {
+    spl_autoload_register(function ($className) {
+        if (strpos($className, 'Technodelight\\JiraGitHubExtension') === false) {
+            return;
+        }
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, str_replace('Technodelight\\JiraGitHubExtension\\', '', $className));
+        if (is_file('./' . $path . '.php')) {
+            require_once './' . $path . '.php';
+        }
+    });
+}
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
-use Technodelight\Jira\Extension\ExtensionInterface;
+namespace Technodelight\JiraGitHubExtension {
 
-class Extension implements ExtensionInterface
-{
-    public function load(array $configs, ContainerBuilder $container)
+    use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+    use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+    use Symfony\Component\Config\FileLocator;
+    use Symfony\Component\DependencyInjection\ContainerBuilder;
+    use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+    use Symfony\Component\DependencyInjection\Reference;
+    use Technodelight\Jira\Extension\ExtensionInterface;
+
+    class Extension implements ExtensionInterface
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources'));
-        $loader->load('services.xml');
+        public function load(array $configs, ContainerBuilder $container)
+        {
+            $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources'));
+            $loader->load('services.xml');
 
-        $def = $container->getDefinition('technodelight.jira.config.integrations.github');
-        $def->setArguments(
-            [isset($configs['github']) ? $configs['github'] : []]
-        );
+            $def = $container->getDefinition('technodelight.jira.config.integrations.github');
+            $def->setArguments(
+                [isset($configs['github']) ? $configs['github'] : []]
+            );
 
-        // add extra renderer to issue transition
-        $def = $container->getDefinition('technodelight.jira.renderer.issue.transitions');
-        $args = $def->getArguments();
-        $args[count($args)] = new Reference('technodelight.jira.renderer.issue.github');
-        $def->setArguments($args);
-    }
+            // add extra renderer to issue transition
+            $def = $container->getDefinition('technodelight.jira.renderer.issue.transitions');
+            $args = $def->getArguments();
+            $args[count($args)] = new Reference('technodelight.jira.renderer.issue.github');
+            $def->setArguments($args);
+        }
 
-    public function configure(): ArrayNodeDefinition
-    {
-        $builder = new TreeBuilder;
-        $node = $builder->root('github');
+        public function configure(): ArrayNodeDefinition
+        {
+            $builder = new TreeBuilder;
+            $node = $builder->root('github');
 
-        $node
-            ->info('GitHub credentials - used to retrieve pull request data, including webhook '
-                . 'statuses. Visit this page to generate a token: '
-                . 'https://github.com/settings/tokens/new?scopes=repo&description=jira+cli+tool')
-            ->addDefaultsIfNotSet()
-            ->children()
+            $node
+                ->info('GitHub credentials - used to retrieve pull request data, including webhook '
+                    . 'statuses. Visit this page to generate a token: '
+                    . 'https://github.com/settings/tokens/new?scopes=repo&description=jira+cli+tool')
+                ->addDefaultsIfNotSet()
+                ->children()
                 ->scalarNode('apiToken')
-                    ->attribute('hidden', true)
-                    ->defaultNull()
+                ->attribute('hidden', true)
+                ->defaultNull()
                 ->end()
                 ->scalarNode('owner')
-                    ->defaultNull()
+                ->defaultNull()
                 ->end()
                 ->scalarNode('repo')
-                    ->defaultNull()
+                ->defaultNull()
                 ->end()
-            ->end()
-        ;
+                ->end()
+            ;
 
-        return $node;
+            return $node;
+        }
     }
+
 }
