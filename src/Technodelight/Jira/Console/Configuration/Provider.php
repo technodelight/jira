8b73fea15af5defcdfcd2dccdbb4b2453fe43ec8
Zsolt Gál <technodelight@gmail.com>
2020-11-02 16:46:33 +0100

    Release 0.9.27 (#99)
    - allow import configs from .jira.yml files
    - other minor fixes

--- src/Technodelight/Jira/Console/Configuration/Provider.php
+++ src/Technodelight/Jira/Console/Configuration/Provider.php
@@ -4,34 +4,20 @@ namespace Technodelight\Jira\Console\Configuration;
 
 class Provider
 {
-    /**
-     * @var DirectoryProvider
-     */
     private $directoryProvider;
-    /**
-     * @var Loader
-     */
     private $loader;
-    /**
-     * @var array|null
-     */
-    private $configs;
 
     public function __construct(DirectoryProvider $directoryProvider, Loader $loader)
     {
-        $this->loader = $loader;
         $this->directoryProvider = $directoryProvider;
+        $this->loader = $loader;
     }
 
     public function get(): array
     {
-//        if (!$this->configs) {
-            $this->configs = $this->loader->load([
-                $this->directoryProvider->project(),
-                $this->directoryProvider->user(),
-            ]);
-//        }
-
-        return $this->configs;
+        return $this->loader->load([
+            $this->directoryProvider->project(),
+            $this->directoryProvider->user(),
+        ]);
     }
 }
