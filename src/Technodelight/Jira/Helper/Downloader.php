5d538dbfc0356a1d2692301f05950d0a769ba766
Zsolt Gál <zsolt.gal@borngroup.com>
2023-08-03 11:44:15 +0200

    fix progress bar

--- src/Technodelight/Jira/Helper/Downloader.php
+++ src/Technodelight/Jira/Helper/Downloader.php
@@ -2,6 +2,7 @@
 
 namespace Technodelight\Jira\Helper;
 
+use CurlHandle;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
@@ -10,9 +11,9 @@ class Downloader
 {
     public function downloadWithCurl(OutputInterface $output, $downloadUrl, $targetFile): bool
     {
-        list($progress, $callback) = $this->progressBarWithProgressFunction($output);
-        /** @var ProgressBar $progress */
-        /** @var callable $callback */
+        $callback = static function ($resource, $downloadTotal, $downloadedBytes) use ($output) {
+            $output->write(sprintf("\033[K" . 'downloaded: %.4fMiB / %.4fMiB', $downloadedBytes, $downloadTotal));
+        };
 
         $f = fopen($targetFile, 'w');
         $ch = $this->initCurl($downloadUrl, $f, $callback);
@@ -24,60 +25,18 @@ class Downloader
         fclose($f);
         umask($old);
 
-        $progress->finish();
         $output->writeln('');
 
         return $err === 0;
     }
 
     /**
-     * @param OutputInterface $output
-     * @return array [ProgressBar, Closure]
-     */
-    public function progressBarWithProgressFunction(OutputInterface $output)
-    {
-        $progress = $this->createProgressBar($output);
-        $progressFunction = static function($resource, $downloadTotal, $downloadedBytes, $upload_size, $uploaded) use ($progress) {
-            static $total = null;
-
-            try {
-                if (null === $total) {
-                    $progress->clear();
-                    $progress->start($downloadTotal);
-                    $progress->setFormat('%bar% %percent%% %remaining%');
-                    $progress->setProgress($downloadedBytes);
-                    $total = $downloadTotal;
-                } else if ($progress->getStartTime() && $downloadedBytes > 0) {
-                    $progress->setProgress($downloadedBytes);
-                }
-            } catch (Throwable $e) {
-                $progress->advance(500);
-            }
-        };
-
-        return [$progress, $progressFunction];
-    }
-
-    private function createProgressBar(OutputInterface $output): ProgressBar
-    {
-        $progress = new ProgressBar($output);
-        $progress->setFormat('%bar% %percent%%');
-        $progress->setBarCharacter('<bg=green> </>');
-        $progress->setEmptyBarCharacter('<bg=white> </>');
-        $progress->setProgressCharacter('<bg=green> </>');
-        $progress->setBarWidth(50);
-        $progress->setRedrawFrequency(500000);
-
-        return $progress;
-    }
-
-    /**
      * @param string $downloadUrl
      * @param resource $f
      * @param callable $callback
-     * @return resource
+     * @return CurlHandle
      */
-    private function initCurl(string $downloadUrl, $f, callable $callback)
+    private function initCurl(string $downloadUrl, $f, callable $callback): CurlHandle
     {
         $ch = curl_init($downloadUrl);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
