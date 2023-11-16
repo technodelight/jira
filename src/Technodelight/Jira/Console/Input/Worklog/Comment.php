45bbe2f0cfcd07a25bd92e7acff02444a06ddf64
Zsolt Gál <zsolt.gal@borngroup.com>
2023-11-16 10:42:42 +0100

    fix worklog comment autocomplete

--- src/Technodelight/Jira/Console/Input/Worklog/Comment.php
+++ src/Technodelight/Jira/Console/Input/Worklog/Comment.php
@@ -1,38 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Technodelight\Jira\Console\Input\Worklog;
 
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
-use Technodelight\GitShell\ApiInterface as Git;
-use Technodelight\GitShell\Branch;
-use Technodelight\GitShell\LogEntry;
-use Technodelight\GitShell\LogMessage;
 use Technodelight\Jira\Api\JiraRestApi\Api;
 use Technodelight\Jira\Connector\HoaConsole\Aggregate;
 use Technodelight\Jira\Connector\HoaConsole\UsernameAutocomplete;
 use Technodelight\Jira\Connector\HoaConsole\Word;
-use Technodelight\Jira\Connector\WorklogHandler;
 use Technodelight\Jira\Domain\Issue;
-use Technodelight\Jira\Domain\User;
 use Technodelight\Jira\Domain\Worklog;
 
 class Comment
 {
-    /**
-     * @var Api
-     */
-    private $api;
-
-    public function __construct(Api $api)
+    public function __construct(private readonly Api $api)
     {
-        $this->api = $api;
     }
 
-    public function read(InputInterface$input, OutputInterface $output, Issue $issue = null, Worklog $worklog = null, $keepDefault = false)
-    {
+    public function read(
+        InputInterface$input,
+        OutputInterface $output,
+        Issue $issue = null,
+        Worklog $worklog = null
+    ): string {
         $q = new QuestionHelper();
         $question = new Question($this->worklogCommentDialogText($worklog->comment(), $worklog));
         $question->setAutocompleterCallback(new Aggregate([
@@ -43,17 +37,7 @@ class Comment
         return $q->ask($input, $output, $question);
     }
 
-    private function buildAutocompleter(Issue $issue, array $words = [])
-    {
-        $autocompleters = [
-            $words ? new Word(array_unique($words)) : null,
-            new UsernameAutocomplete($issue, $this->api)
-        ];
-
-        return new Aggregate(array_filter($autocompleters));
-    }
-
-    private function fetchWordsList(Issue $issue)
+    private function fetchWordsList(Issue $issue): array
     {
         $list = $issue->comments();
         $list[] = $issue->description();
@@ -62,10 +46,10 @@ class Comment
         return $this->collectWords($list);
     }
 
-    private function collectWords(array $texts)
+    private function collectWords(array $texts): array
     {
         $words = [];
-        foreach ($texts as $text) {
+        foreach (array_filter($texts) as $text) {
             foreach ($this->collectAutocompleteableWords($text) as $word) {
                 $words[] = $word;
             }
@@ -74,7 +58,7 @@ class Comment
         return array_unique($words);
     }
 
-    private function collectAutocompleteableWords($text)
+    private function collectAutocompleteableWords(string $text): array
     {
         $text = preg_replace('~[^a-zA-Z0-9\s\']+~', '', $text);
 
@@ -87,12 +71,7 @@ class Comment
         });
     }
 
-    /**
-     * @param string $defaultMessage
-     * @param Worklog|null $worklog
-     * @return string
-     */
-    private function worklogCommentDialogText($defaultMessage, $worklog = null)
+    private function worklogCommentDialogText(string $defaultMessage, ?Worklog $worklog = null): string
     {
         $verb = $worklog ? 'left as' : 'set to';
         $comment = $worklog ? $worklog->comment() : $defaultMessage;
